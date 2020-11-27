2abfbcf3ae9d25fe0b44aa58ba3b30d063ced25f
Maxim Tarasov <yousan4ik@gmail.com>
2020-11-27 20:10:09 +0300

    GitCli: Fix repository creating

--- git-cli/src/main/java/com/mwguy/vgit/commands/GitInitCommand.java
+++ git-cli/src/main/java/com/mwguy/vgit/commands/GitInitCommand.java
@@ -4,6 +4,7 @@ import com.mwguy.vgit.data.GitRepository;
 import com.mwguy.vgit.exceptions.GitException;
 import lombok.Builder;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -23,8 +24,13 @@ public class GitInitCommand implements GitCommand<Void> {
         }
 
         try {
+            File processDirectory = repository.getPath().toAbsolutePath().toFile();
+            if (!processDirectory.isDirectory()) {
+                processDirectory.mkdirs();
+            }
+
             ProcessBuilder processBuilder = new ProcessBuilder(command);
-            processBuilder.directory(repository.getPath().toAbsolutePath().toFile());
+            processBuilder.directory(processDirectory);
             repository.getEnvironmentResolver().resolve().forEach(processBuilder.environment()::put);
 
             Process process = processBuilder.start();
