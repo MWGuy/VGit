305ff6a9d9fc9884123505dee73aae8704f9a5ca
Maxim Tarasov <yousan4ik@gmail.com>
2020-11-29 14:33:30 +0300

    Frontend: Little updates in CredentialsInputPage.tsx

--- frontend/src/components/pages/CredentialsInputPage.tsx
+++ frontend/src/components/pages/CredentialsInputPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Card, HTMLDivProps} from "@blueprintjs/core";
 
 import logo from "../../images/logoWithText.svg";
+import { useHistory } from "react-router-dom";
 
 interface CredentialsInputPageProps extends HTMLDivProps {
     description: string;
@@ -9,8 +10,12 @@ interface CredentialsInputPageProps extends HTMLDivProps {
 }
 
 export default (props: CredentialsInputPageProps) => {
+    const history = useHistory();
+
     return <div className="credentials-input-page-container">
-        <img alt="logo" src={logo} width={42}/>
+        <a onClick={() => history.push("/")}>
+            <img alt="logo" src={logo} width={42}/>
+        </a>
         {props.callout && <div className="credentials-input-page-callout">{props.callout}</div>}
         <h3>{props.description}</h3>
         <Card className="credentials-input-page-card" elevation={1}>
